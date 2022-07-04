7e3fb3388b0b5cdd55f72391fe3105532efafa39
Alex Tomas <atomasllimos@gmail.com>
2022-07-04 16:31:56 +0200

    Updated ./resumes/part5-notes.txt

--- src/components/Note.test.js
+++ src/components/Note.test.js
@@ -13,6 +13,13 @@ test('renders content', () => {
 
   component.getByText('This is a test')
   component.getByText('make not important')
+
+  // Muestra que se esta renderizando
+  // component.debug()
+
+  // Ver si un elemento en especifico se ha renderizado
+  // const li = component.container.querySelector('li')
+  // console.log(prettyDOM(li))
 })
 
 test('clicking the button calls event handler once', () => {
@@ -21,12 +28,18 @@ test('clicking the button calls event handler once', () => {
     important: true
   }
 
+  // Hacemos que se pase por la funcion que se ejecuta en el componente cuando se realiza click en el button
   const mockHandler = jest.fn()
 
+  // Renderizamos el componente
   const component = render(<Note note={note} toggleImportance={mockHandler} />)
 
+  // Selecionamos el button del componente renderizado Note
   const button = component.getByText('make not important')
+
+  // Hacemos que en el componente renderizado se haga click en el button seleccionado previamente
   fireEvent.click(button)
 
+  // Esperamos que al haberse realizado un click en el button se ejecute el mock 1 vez
   expect(mockHandler).toHaveBeenCalledTimes(1)
 })
